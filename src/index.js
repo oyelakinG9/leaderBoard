ca4eb2fe8fa57c37630c572afa4854f8adc33ab6
oyelakinG9 <oyelakinr37@gmail.com>
2022-01-26 17:34:35 +0100

    Modify index file

--- src/index.js
+++ src/index.js
@@ -12,18 +12,7 @@ let scoreRecord = [
 ];
 
 const scores = document.querySelector('#scores');
-// function chore() {
-//   Tasks.forEach((task) => {
-//     workList.innerHTML += `<li class="item-list" data-key="${task.index}">
-//           <div class="checkList">
-//               <input type="checkbox" name="checkbox" id="checkbox" data-key="${task.index}">
-//               <input type="text" id="inputList"  value="${task.description}">
-//               <button data-key="${task.index}" class="move-item"><i class="fas fa-ellipsis-v"></i></button>
-//           </div>
-//       </li>`;
-//   });
-// }
-function scoreSheet() {
+const scoreSheet = () => {
   scoreRecord.forEach((score) => {
     scores.innerHTML += `<tr><td>${score.name}</td><td>${score.score}</td></tr>`;
   });
