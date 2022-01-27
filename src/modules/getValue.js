fddb2e491ed676dd2f504145becb574962014870
oyelakinG9 <oyelakinr37@gmail.com>
2022-01-27 18:53:05 +0100

    Check linters errors

--- src/modules/getValue.js
+++ src/modules/getValue.js
@@ -1,7 +1,7 @@
 const scoreValue = (name, score) => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `<td>${name}:</td><td>${score}</td>`;
-    return tr;
-  };
+  const tr = document.createElement('tr');
+  tr.innerHTML = `<td>${name}:</td><td>${score}</td>`;
+  return tr;
+};
 
 export default scoreValue;
